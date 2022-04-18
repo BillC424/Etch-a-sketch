022f1a9143ec28836af3eba822575f96bd89b8ef
BillC424 <wcurran44@gmail.com>
2022-04-17 23:21:23 -0400

    Added prompt to clear button to get number input from user for new grid size

--- script.js
+++ script.js
@@ -18,7 +18,18 @@ const filledSquare = document.getElementsByClassName("black");
 const clearSquare = Array.from(filledSquare);
 for (let i = 0; i < clearSquare.length; i++) {
     clearSquare[i].classList.remove('black');   
-} }
+} 
+
+const newGrid = window.prompt("Enter a number for the number of squares per side of the new grid");
+
+if (newGrid > 100) {
+    alert("That's too many squares!!! Enter a number lower than 100");
+    clearGrid();
+}
+return newGrid;
+}
+
+
 
 clearButton.addEventListener ("click", clearGrid )
 
