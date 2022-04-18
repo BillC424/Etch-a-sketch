b6b5c9e6531bd0cb8896310f832d99f2f55a9c25
BillC424 <wcurran44@gmail.com>
2022-04-17 21:09:13 -0400

    Modified create div function and CSS to make the divs appear as a 16 by 16 visible grid

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const grid = document.getElementById('sketch-container');
 
 function createGridDiv() {
     const div = document.createElement('div');
+    div.classList.add('square');
     grid.appendChild(div);
 }
 
-for (i=0; i < 257; i++) {
+for (i=0; i < 256; i++) {
 createGridDiv();
 
 }
