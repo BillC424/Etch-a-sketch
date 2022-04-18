85bca95a20d6026d19a629dd5ad3f39a89112eba
BillC424 <wcurran44@gmail.com>
2022-04-17 22:02:56 -0400

    Modified createGridDiv() method to change background color of grid squares using an eventListener to add a class with a black background

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ function createGridDiv() {
     const div = document.createElement('div');
     div.classList.add('square');
     grid.appendChild(div);
+    div.addEventListener ("mouseenter", function(){ div.classList.add('black'); } )
 }
+
 
 for (i=0; i < 256; i++) {
 createGridDiv();
-
 }
-
-
