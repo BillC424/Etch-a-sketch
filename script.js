8e682a23ac39796b1bfe757afe461cdbafea9689
BillC424 <wcurran44@gmail.com>
2022-04-17 23:05:02 -0400

    Added button to clear grid when clicked

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const grid = document.getElementById('sketch-container');
+const clearButton = document.getElementById('clear-button');
 
 
 function createGridDiv() {
@@ -8,7 +9,18 @@ function createGridDiv() {
     div.addEventListener ("mouseenter", function(){ div.classList.add('black'); } )
 }
 
-
 for (i=0; i < 256; i++) {
-createGridDiv();
-}
+    createGridDiv();
+    }
+
+function clearGrid() {
+const filledSquare = document.getElementsByClassName("black");
+const clearSquare = Array.from(filledSquare);
+for (let i = 0; i < clearSquare.length; i++) {
+    clearSquare[i].classList.remove('black');   
+} }
+
+clearButton.addEventListener ("click", clearGrid )
+
+
+
