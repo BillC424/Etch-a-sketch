e54fa83eaaf16d7d328b3aa8e9f7b3908100c758
BillC424 <wcurran44@gmail.com>
2022-05-03 23:48:27 -0400

    Created named function to change eventListener for changing squares to black so that the rainbow mode button could remove the eventListener. Also created an eventListener in the rainbow button function to change the background of the squares to green instead. This will be replaced with code to make the background of each square a random color when the mouse passes over.

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 const grid = document.getElementById('sketch-container');
 const clearButton = document.getElementById('clear-button');
+const rainbowButton = document.getElementById('rainbow-mode');
+
+
+function addBlack(e) {
+    const blackSquare = e.target;
+    blackSquare.classList.add('black');
+}
+
+function addGreen(e) {
+    const greenSquare = e.target;
+    greenSquare.classList.add('green');
+}
 
 function createGridDiv(gridSize) { for (i=0; i < gridSize * gridSize; i++) {
     grid.style.gridTemplateColumns = 'repeat (newGrid, auto) ;';
-    const div = document.createElement('div');
-    div.classList.add('square');
-    grid.appendChild(div);
-    div.addEventListener ("mouseenter", function(){ div.classList.add('black'); } ) }
+    const square = document.createElement('div');
+    square.classList.add('square');
+    grid.appendChild(square);
+    square.addEventListener ("mouseenter", addBlack  ) 
+
+    
+
+}
     
 }
+
 
 createGridDiv(16);
 
 function clearGrid() {
-const filledSquare = document.getElementsByClassName("square");
-const clearSquare = Array.from(filledSquare);
-for (let i = 0; i < clearSquare.length; i++) {
-    grid.removeChild(clearSquare[i]);
+const filledSquare = Array.from(document.getElementsByClassName('square'));
+for (let i = 0; i < filledSquare.length; i++) {
+    grid.removeChild(filledSquare[i]);
 } 
 
 let newGrid = window.prompt("Enter a number for the number of squares per side of the new grid");
@@ -44,4 +60,24 @@ createGridDiv(newGrid);
 grid.style.gridTemplateColumns = `repeat(${newGrid}, auto)`;
 }
 
+function rainbowGrid() { 
+    
+    const square = Array.from(document.getElementsByClassName('square'));
+    for (i=0; i < square.length; i++) {
+    
+        square[i].removeEventListener ("mouseenter", addBlack  ) 
+        square[i].addEventListener ("mouseenter", addGreen)  
+        
+        //square[i].addEventListener ("mouseenter", function(){ square[i].classList.add('green'); } )
+        
+    }
+    
+    }
+
+
 clearButton.addEventListener ("click", e => clearGrid() )
+
+rainbowButton.addEventListener ("click", e => rainbowGrid() )
+
+
+
